50e2b2de653ce151ba33757003af1570a54952be
Praful Aswale <prafulcode15@gmail.com>
2024-05-10 12:42:35 +0530

    sign-up page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function page() {
+const SignInForm = () => {
   return (
-    <div>page</div>
+    <div>SignInForm</div>
   )
 }
 
-export default page
+export default SignInForm
